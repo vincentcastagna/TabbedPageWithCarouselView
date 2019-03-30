9d58d70980fd71278b67173deae4ec0ca1117006
Umar3x <castagna.vin@gmail.com>
2019-03-30 13:17:32 +0100

    first commit

---
+++ App1/App1/ViewModels/AboutViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using App1.Models;
+using Xamarin.Forms;
+
+namespace App1.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+
+        private ObservableCollection<Item> _items;
+        public ObservableCollection<Item> Items
+        {
+            get => _items;
+            set => SetProperty(ref _items, value);
+        }
+
+        public AboutViewModel()
+        {
+            Title = "About";
+
+           Items = new ObservableCollection<Item>();
+            Items.Add(new Item{ Id = "blablabla", Description = "SomeDescription", Text = "Some text"});
+            Items.Add(new Item { Id = "bliblibli", Description = "Some Description 2", Text = "Some text 2" });
+            Items.Add(new Item { Id = "blobloblo", Description = "Some Description 3", Text = "Some text 3" });
+        }
+
+    }
+}
